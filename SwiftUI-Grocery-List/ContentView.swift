4ff8584c325ffe1584c6cd1152239fcd5448a606
monir <mhelalee@wingsfin.com>
2025-03-15 13:31:00 +0600

    Initial Commit

---
+++ SwiftUI-Grocery-List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Grocery-List
+//
+//  Created by Monir Haider Helalee on 15/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Grocery-List/SwiftUI_Grocery_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Grocery_ListApp.swift
+//  SwiftUI-Grocery-List
+//
+//  Created by Monir Haider Helalee on 15/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Grocery_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
